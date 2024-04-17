724eda12eeda88ef91beb5300727c52312f4735a
Daryl Bacurin <darylbacurin14@gmail.com>
2024-04-17 22:27:10 +0800

    Rating function done [hover, active, click event, rating form]

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
+// Select the currently clicked element with the class 'clicked'
+let currentClicked = document.querySelector('.clicked');
+
 document.addEventListener('click', e => {
-    const ratingClicked = e.target.matches('.ratingForm_rating_container label');
-    if (ratingClicked) {
-        console.log("test");
+    // Check if the user clicked an element with the class 'ratingForm_rating_container label' (label/rating)
+    if (e.target.matches('.ratingForm_rating_container label')) {
+
+        // Remove the 'clicked' class from the previously clicked element
+        currentClicked.classList.remove('clicked');
+
+        // Toggle the 'clicked' class on the newly clicked element
+        e.target.classList.toggle('clicked');
+
+        // Update the reference to the currently clicked element
+        currentClicked = e.target;
     }
-})
+});
 
 document.getElementById('ratingForm').addEventListener('submit', e => {
     e.preventDefault();
