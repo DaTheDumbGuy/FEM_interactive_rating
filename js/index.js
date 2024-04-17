e7c060414d4247b209c2bbeb1011b82325da1f6b
Daryl Bacurin <darylbacurin14@gmail.com>
2024-04-18 00:06:37 +0800

    Final version 1.3

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-// Select the currently clicked element with the class 'clicked'
-let currentClicked = document.querySelector('.clicked');
+let currentClicked = null;
 
 document.addEventListener('click', e => {
     // Check if the user clicked an element with the class 'ratingForm_rating_container label' (label/rating)
     if (e.target.matches('.ratingForm_rating_container label')) {
 
-        // Remove the 'clicked' class from the previously clicked element
-        currentClicked.classList.remove('clicked');
+        // If currentClicked is not null, remove the 'clicked' class from it
+        if (currentClicked) {
+            currentClicked.classList.remove('clicked');
+        }
 
         // Toggle the 'clicked' class on the newly clicked element
         e.target.classList.toggle('clicked');
@@ -16,9 +17,25 @@ document.addEventListener('click', e => {
     }
 });
 
+
 document.getElementById('ratingForm').addEventListener('submit', e => {
     e.preventDefault();
 
-    const selectedRating = document.querySelector('input[name="rating"]:checked').value;
-    console.log('Selected rating:', selectedRating);
+    // Check if any radio button with name "rating" is checked
+    const selectedRadio = document.querySelector('input[name="rating"]:checked');
+
+    // Hide the rating container and display the "thank you" message
+    document.querySelector('.rating_container').classList.add('hide');
+    document.querySelector('.rating_result').classList.add('thank_you_state');
+
+    // Get the element where the selected rating will be displayed
+    const selectedRatingResult = document.querySelector('.rating_user');
+
+    if (selectedRadio) {
+        // If a rating is selected, display the selected rating
+        selectedRatingResult.textContent = selectedRadio.value;
+    } else {
+        // If no rating is selected, display "0" to indicate no rating
+        selectedRatingResult.textContent = '0';
+    }
 });
